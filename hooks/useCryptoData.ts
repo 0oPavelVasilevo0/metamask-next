149bf27b31712833ac654e75510eba2672f7b8a8
Pavel Vasilev <karterpahan@gmail.com>
2024-03-19 22:22:20 +0100

    fix useCryptoData

---
+++ hooks/useCryptoData.ts
@@ -0,0 +1,42 @@
+'use client'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface CryptoData {
+    bitcoin?: {
+        usd?: number;
+        eur?: number;
+        rub?: number;
+    };
+    ethereum?: {
+        usd?: number;
+        eur?: number;
+        rub?: number;
+    };
+    tether?: {
+        usd?: number;
+        eur?: number;
+        rub?: number;
+    };
+}
+
+const useCryptoData = () => {
+    const [cryptoData, setCryptoData] = useState<CryptoData>({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether&vs_currencies=usd,eur,rub');
+                setCryptoData(response.data);
+            } catch (error) {
+                console.error('Error fetching crypto data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return cryptoData;
+};
+
+export default useCryptoData;
