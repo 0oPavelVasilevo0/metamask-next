63b1d0e0284b1bd92d877b4bb3aa52d3ea674b75
Pavel Vasilev <karterpahan@gmail.com>
2024-03-22 19:09:01 +0100

    add binancecoin

--- hooks/useCryptoData.ts
+++ hooks/useCryptoData.ts
@@ -3,6 +3,11 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface CryptoData {
+    binancecoin?: {
+        usd?: number;
+        eur?: number;
+        rub?: number;
+    };
     bitcoin?: {
         usd?: number;
         eur?: number;
@@ -26,7 +31,7 @@ const useCryptoData = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether&vs_currencies=usd,eur,rub');
+                const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin,bitcoin,ethereum,tether&vs_currencies=usd,eur,rub');
                 setCryptoData(response.data);
             } catch (error) {
                 console.error('Error fetching crypto data:', error);
