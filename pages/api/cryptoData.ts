6cd0239109d80f91045479f56c369c5520f1410c
Pavel Vasilev <karterpahan@gmail.com>
2024-03-29 15:53:13 +0100

    fix cryptoData

--- pages/api/cryptoData.ts
+++ pages/api/cryptoData.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export default async function handler(_req: any, res: any) {
     try {
-        const apiUrl = process.env.NEXT_APP_API_URL
-        if (!apiUrl) {
-            console.error('API URL is not defined');
-            return;
-        }
-        const response = await axios.get(apiUrl);
+        // const apiUrl = process.env.NEXT_APP_API_URL
+        // if (!apiUrl) {
+        //     console.error('API URL is not defined');
+        //     return;
+        // }
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin,bitcoin,ethereum,tether&vs_currencies=usd,eur,rub');
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching data:', error);
